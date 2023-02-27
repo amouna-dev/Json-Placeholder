87e9f920d080b71d6c484850a3c8009126a78655
amouna-dev <amouna_1387@yahoo.fr>
2023-02-27 09:34:44 +0100

    commit

--- script.js
+++ script.js
@@ -2,38 +2,27 @@ let users_list = document.getElementById("users")
 let posts_list = document.getElementById("posts")
 
 function getAllUsers(){
-    let request = new XMLHttpRequest()
-    request.open("GET", "https://jsonplaceholder.typicode.com/users")
-    request.responseType = "json"
-    request.send()
-    request.onload = function(){
 
-        if(request.status >= 200 && request.status < 300){
-            let users = request.response
-            
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response) => response.json())
+        .then((users) => {
             for (user of users) {
                 users_list.innerHTML += `<div class="user_bloc" id="user_${user.id}" onclick="userClicked(${user.id}, this)">
                     <h3 class="user_name">${user.name}</h3> 
                     <p class="user_email">${user.email}</p>
                 </div>` 
-             
+            
             }
-        }else
-            alert("Error")
-        
-    }
+        })
+
 }
 getAllUsers()
 
 function getPostsByUser(userId) {
-    let request = new XMLHttpRequest()
-    request.open("GET", "https://jsonplaceholder.typicode.com/posts?userId="+userId)
-    request.responseType = "json"
-    request.send()
-    request.onload = function(){
 
-        if(request.status >= 200 && request.status < 300){
-            let posts = request.response
+    fetch('https://jsonplaceholder.typicode.com/posts?userId='+userId)
+        .then((response) => response.json())
+        .then((posts) => {
             posts_list.innerHTML = ""
 
             for (post of posts) {
@@ -42,11 +31,7 @@ function getPostsByUser(userId) {
                 <p class="post_body"> ${post.body} </p>
              </div>`  
             }
-
-        }else
-            alert("Error")
-        
-    }
+        })
 }
 
 getPostsByUser(1)
